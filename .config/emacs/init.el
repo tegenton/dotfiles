437c41afd400ba2be34af67b35ebaf580c88b53f
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2021-11-25 11:17:00 -0500

    basic evil mode

---
+++ .config/emacs/init.el
@@ -0,0 +1,24 @@
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+	                  '("melpa" . "https://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+;; Disable <file>~ temp files
+(setq make-backup-files nil)
